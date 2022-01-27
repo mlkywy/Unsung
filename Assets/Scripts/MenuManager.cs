581b1c49c2a2eab55922d8b5618c55904c8f3686
alsheikhiu <alsheikh@iu.edu>
2022-01-26 21:38:45 -0500

    main menu buttons working with key up/key down

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject[] frame;
+    [SerializeField] private GameObject[] frame;
+    [SerializeField] private GameObject[] choices;
 
 
     private void Start()
@@ -20,6 +22,7 @@ public class MenuManager : MonoBehaviour
         {
             frame[0].SetActive(false);
             frame[1].SetActive(true);
+            StartCoroutine(SelectFirstChoice());
         }
 
         if (Input.GetButtonDown("Cancel") && !frame[0].activeInHierarchy)
@@ -30,15 +33,23 @@ public class MenuManager : MonoBehaviour
     }
 
 
-    public void StartGame()
+    private void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
-    public void QuitGame() 
+    private void QuitGame() 
     {
         Debug.Log("Quit!");
         Application.Quit();
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        // event system requires that we clear it first, then wait for at least one frame before setting current selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
     }
 }

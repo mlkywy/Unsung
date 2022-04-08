3712b5a1fbb85db65246f127422495e5849e7e60
alsheikhiu <alsheikh@iu.edu>
2022-04-08 18:51:02 -0400

    can switch to battle scene from main scene

--- Assets/Scripts/NewBattleSystem/BattleLauncherDemo.cs
+++ Assets/Scripts/NewBattleSystem/BattleLauncherDemo.cs
@@ -11,4 +11,20 @@ public class BattleLauncherDemo : MonoBehaviour
     {
         launcher.PrepareBattle(enemies, players);
     }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+           Launch();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+           
+        }
+    }
 }

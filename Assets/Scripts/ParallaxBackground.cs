59462ff6a3e2f0a8f67a8c2e61cf853d05a45d89
alsheikhiu <alsheikh@iu.edu>
2022-04-21 18:44:34 -0400

    parallax effect

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-   [SerializeField] private Transform cameraTransform;
+   [SerializeField] private Vector2 parallaxEffectMultiplier;
+
+   private Transform cameraTransform;
    private Vector3 lastCameraPosition;
+
+   private float textureUnitSizeX;
 
    private void Start()
    {
+       cameraTransform = Camera.main.transform;
        lastCameraPosition = cameraTransform.position;
+
+       Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+       Texture2D texture = sprite.texture;
+
+       textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+
    }
 
-   private void LateUpdate()
+   private void FixedUpdate()
    {
        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-       transform.position += deltaMovement;
+       transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
        lastCameraPosition = cameraTransform.position;
+
+       // this function doesn't seem to work 
+       if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+       {
+           Debug.Log("repeat!");
+           float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+           transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+       }
    }
 
 }
